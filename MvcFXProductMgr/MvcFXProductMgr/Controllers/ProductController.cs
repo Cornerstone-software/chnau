f64f7824c9f5ea135f2534070ec2e60d03e656d1
gerena <jinqiaoying126@126.com>
2018-10-29 12:44:22 +0800

    Create project first by jinqiaoying126@126.com
    Create project first by@jinqiaoying126@126.com
    
    Co-Authored-By: 九五子 <273690@qq.com>

---
+++ MvcFXProductMgr/MvcFXProductMgr/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFXProductMgr.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult GetProduct()
+        {
+            return View();
+        }
+        
+        public ActionResult AddProducts()
+        {
+            return View();
+        }
+        public ActionResult DeleteProducts()
+        {
+            return View();
+        }
+        public ActionResult ChangeProducts()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ MvcFXProductMgr/MvcFXProductMgr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcFXProductMgr.MvcApplication" Language="C#" %>
