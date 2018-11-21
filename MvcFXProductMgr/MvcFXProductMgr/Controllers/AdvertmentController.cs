83ed069813a1839e803498f3101b1d1df67612f2
gerena <jinqiaoying126@126.com>
2018-11-21 11:17:20 +0800

    新增了广告和公司信息部分的controllers and views

---
+++ MvcFXProductMgr/MvcFXProductMgr/Controllers/AdvertmentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFXProductMgr.Controllers
+{
+    public class AdvertmentController : Controller
+    {
+        //
+        // GET: /Advertment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 创建新的广告信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CreateNewAdvertment()
+        {
+            ViewBag.Title = "新增广告信息";
+            return View("CreateOrUpdateAdvertment");
+        }
+    }
+}
---
+++ MvcFXProductMgr/MvcFXProductMgr/Controllers/CompanyController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFXProductMgr.Controllers
+{
+    public class CompanyController : Controller
+    {
+        //
+        // GET: /Company/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+       
+        /// <summary>
+        /// 创建新的公司信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CreateNewCompany()
+        {
+          
+            ViewBag.Title = "新建公司信息";
+            return View("CreateOrUpdateCompany");
+        }
+
+        public ActionResult UpdateCompany()
+        {
+
+            ViewBag.Title = "更新公司信息";
+            return View("CreateOrUpdateCompany");
+        }
+        public ActionResult DedelteCompany()
+        {
+            return View();
+        }
+
+    }
+}
