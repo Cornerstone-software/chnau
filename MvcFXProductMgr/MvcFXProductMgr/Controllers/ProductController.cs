5877894ec9a7caf12eb27dee44759d7ec185214f
gerena <jinqiaoying126@126.com>
2018-11-22 15:09:17 +0800

    批量上传产品的部分功能：读取excel文件，读取到的数据显示（难点：视图显示的数据列表如何传递到控制器）

--- MvcFXProductMgr/MvcFXProductMgr/Controllers/ProductController.cs
+++ MvcFXProductMgr/MvcFXProductMgr/Controllers/ProductController.cs
@@ -40,14 +40,29 @@ namespace MvcFXProductMgr.Controllers
         public ActionResult Upload()
             
         {
+          
             string strFileName = "";
             string strSeverDataPath = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/");
             string strSaveFileName = "";
            
             HttpPostedFileBase uploadfile = Request.Files[0];
+            string strCompany = Request["Company"];
+            string[] arrCompany= strCompany.Split(':');
+            string strCompanyId=arrCompany[0];
+            string strCompanyName = arrCompany[1];
+
+            string strTestingOrg = Request["TestingOrg"];
+            string[] arrTestingOrg = strTestingOrg.Split(':');
+            string strTestingOrgId=arrTestingOrg[0];
+            string strTestingOrgName = arrTestingOrg[1];
+
+            string strStandard = Request["Standard"];
+            string strCategory = Request["Category"];
+            
             Stream st = null;
             if (uploadfile != null && uploadfile.ContentLength > 0)
             {
+                ///先保存上传的文件到server,begining
                 strFileName = uploadfile.FileName;
                 st = uploadfile.InputStream;
                 strSaveFileName = strSeverDataPath + strFileName;
@@ -55,20 +70,56 @@ namespace MvcFXProductMgr.Controllers
                 {
                     uploadfile.SaveAs(strSaveFileName);
                 }
-                
+                ///先保存上传的文件到server,ending
+
                 ExcelHelper excelobj = new ExcelHelper(strSaveFileName);
                 DataTable dt = excelobj.GetDataTable("sheet1", true);
-                return Content(dt.Rows.Count.ToString());
+                DataTable newdt = dt.Copy();
+
+                DataColumn dcCompanyId = new DataColumn("CompanyId", typeof(string));
+                dcCompanyId.DefaultValue = strCompanyId;
+                newdt.Columns.Add(dcCompanyId);
+
+                DataColumn dcCompanyName = new DataColumn("CompanyName", typeof(string));
+                dcCompanyName.DefaultValue = strCompanyName;
+                newdt.Columns.Add(dcCompanyName);
+
+                DataColumn dcTestingOrgId = new DataColumn("TestingOrgId",typeof(Int32));
+                dcTestingOrgId.DefaultValue = Int32.Parse(strTestingOrgId);
+                newdt.Columns.Add(dcTestingOrgId);
+                DataColumn dcTestingOrgName = new DataColumn("TestingOrgName", typeof(string));
+                dcTestingOrgName.DefaultValue = strTestingOrgName;
+                newdt.Columns.Add(dcTestingOrgName);
+
+                DataColumn dcCategory = new DataColumn("Category", typeof(string));
+                dcCategory.DefaultValue = strCategory;
+                newdt.Columns.Add(dcCategory);
+
+                DataColumn dcStandard = new DataColumn("Standard", typeof(string));
+                dcStandard.DefaultValue = strStandard;
+                newdt.Columns.Add(dcStandard);
+
+                //ViewData.Model = newdt.AsEnumerable();
+                return View(newdt);
+               
+
             }
             else
             {
                 return Content("Empty excel");
-            }
-
-          
-            
-           
+            }       
                 
         }
+    [HttpPost]
+      public ActionResult Save()
+      {
+          string strProductName = "";
+          for (int i = 0; i < model.Rows.Count; i++)
+          {
+              strProductName += model.Rows[i]["品名"] + "<br/>";
+          }
+          return Content(strProductName);
+          //return RedirectToAction("AddProducts","Product");
+      }
     }
 }
--- MvcFXProductMgr/MvcFXProductMgr/Scripts/js/main.js
+++ MvcFXProductMgr/MvcFXProductMgr/Scripts/js/main.js
@@ -1,47 +1,29 @@
-$(function () {
-
+﻿$(function () {
     $(".leftMenu-list-header").click(function (e) {
-
         if ($(this).parent().siblings().hasClass("hover")) {
             $(this).parent().siblings().removeClass("hover");
             $(this).parent().siblings().children("h3").children("i.menu-onOff").removeClass("glyphicon-menu-up");
             $(this).parent().siblings().children("h3").children("i.menu-onOff").addClass("glyphicon-menu-down");
-
         }
-
         if ($(this).parent().hasClass("hover")) {
             $(this).parent().removeClass("hover");
             $(this).parent().children("h3").children("i.menu-onOff").removeClass("glyphicon-menu-up");
             $(this).parent().children("h3").children("i.menu-onOff").addClass("glyphicon-menu-down");
-
         }
         else {
             $(this).parent().addClass("hover");
             $(this).parent().children("h3").children("i.menu-onOff").removeClass("glyphicon-menu-down");
             $(this).parent().children("h3").children("i.menu-onOff").addClass("glyphicon-menu-up");
         }
-
     });
-
-    //get the real filepath 
-    function getPath(obj) {
-        if (obj) {
-            //ie
-            if (window.navigator.userAgent.indexOf("MSIE") >= 1) {
-                obj.select();
-                return document.selection.createRange().text;
-            }
-            //firefox
-            else if (window.navigator.userAgent.indexOf("Firefox") >= 1) {
-                if (obj.files) {
-                    return obj.files.item(0).getAsDataURL();
-                }
-                return obj.value;
-            }
-            return obj.value;
+    $(".file-box input[name='submit']").click(function () {
+        if ($("#fileField").val().length < 1) {
+            alert("请选择要上传的Excel文件");
+            return false;
         }
-    }
-
+        if ($("input[name='Standard']:checked").length < 1) {
+            alert("请选择执行标准");
+            return false;
+        }
+    });
 });
-
-
